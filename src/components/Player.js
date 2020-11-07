74db686d043e84cb6a037bc99996ad7aa6b94150
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-07 10:20:39 -0600

    adds is audio playing logic

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlay,
+  faPause,
   faAngleLeft,
   faAngleRight,
 } from '@fortawesome/free-solid-svg-icons';
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+  const audioElementRef = useRef();
+
+  const playSongHandler = () => {
+    if (isAudioPlaying()) {
+      audioElementRef.current.pause();
+    } else {
+      audioElementRef.current.play();
+    }
+  };
+
+  const isAudioPlaying = () => {
+    return !audioElementRef.current.paused;
+  };
+
   return (
     <div className='player'>
       <div className='time-control'>
@@ -16,13 +31,19 @@ const Player = () => {
       </div>
       <div className='play-control'>
         <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
-        <FontAwesomeIcon className='play' size='2x' icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className='play'
+          size='2x'
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className='skip-forward'
           size='2x'
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioElementRef} src={currentSong.audio}></audio>
     </div>
   );
 };
